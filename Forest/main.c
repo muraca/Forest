5aceee19ca773465b380a556b1028d6c7cbc57d2
muraca <=>
2020-05-23 11:07:01 +0200

    Initial Commit

---
+++ Forest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Forest
+//
+//  Created by Matteo Muraca on 23/05/2020.
+//  Copyright © 2020 Matteo Muraca. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
